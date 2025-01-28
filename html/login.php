237f19141d6af286d35f43e08f56733e0d794a6f
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-01-28 18:37:55 -0300

    feat: criando login.php

---
+++ html/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/login.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-login">
+        <form action="">
+            <h1>Login</h1>
+            <label for="usuário">Usuário:</label>
+            <br>
+            <input type="text" name="email" placeholder="Usuário" id="usuario">
+            <br><br> 
+            <label for="senha">Senha:</label>
+            <br>
+            <input type="password" name="senha" placeholder="senha" id="senha">
+            <br><br>
+            <div class="container-button">
+                <a href="./cadastro.php">Cadastrar-se</a>
+                <input type="submit" name="submit" id="submit" value="Entrar"> 
+            </div> 
+        </form>
+    </div>
+</body>
+</html>
