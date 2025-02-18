1ec49dbc2528efda18ae20c5526bb32c61912a1c
Felipe Romero Alves da Silva <fras2@discente.ifpe.edu.br>
2025-02-18 18:41:11 -0300

    fix: cadastromonetarioproduto

---
+++ html/cadastromonetarioproduto.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/cadastromonetarioproduto.css">
+    <link rel="shortcut icon" href="../imgs/MQA_blue.svg" type="image/x-icon">
+    <title>Cadastro</title>
+</head>
+<body>
+   <div class="app">
+      <div class="header">
+        <div class="nav">
+            <img class="mqa" src="../imgs/MQA_white.svg" alt="">
+        </div>
+    </div>
+</body>
+     
