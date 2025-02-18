872e1a141d91157ace84a634f594bdcee9e96ac7
Felipe Romero Alves da Silva <fras2@discente.ifpe.edu.br>
2025-02-18 19:16:46 -0300

    fit: imagem do perfil (area de edição)

--- html/cadastromonetarioproduto.php
+++ html/cadastromonetarioproduto.php
@@ -6,13 +6,17 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/cadastromonetarioproduto.css">
     <link rel="shortcut icon" href="../imgs/MQA_blue.svg" type="image/x-icon">
-    <title>Cadastro</title>
+    <title>Editar Cadastro</title>
 </head>
 <body>
-   <div class="app">
-      <div class="header">
-        <div class="nav">
-            <img class="mqa" src="../imgs/MQA_white.svg" alt="">
+    <div class="app">
+        <div class="header">
+            <div class="nav">
+                <img class="mqa" src="../imgs/MQA_white.svg" alt="">
+            </div>
+        </div>  
+        <div class="img-perf-box">
+            <img class="imagem-do-perfil" src="../imgs/banner.png">
         </div>
     </div>
 </body>
