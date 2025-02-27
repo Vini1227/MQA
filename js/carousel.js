2d7d9c25edb63a841ec685b1b4bf3dd4f3e57779
GuilhermeMth <Guilhermemfranca06@gmail.com>
2025-02-26 23:50:45 -0300

    feat: script do carrossel da pagina index

---
+++ js/carousel.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.querySelectorAll('.carousel-slide');
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active-slide');
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    if (slideIndex < 1) { slideIndex = slides.length }
+    slides[slideIndex - 1].classList.add('active-slide');
+    setTimeout(showSlides, 10000); // Muda a imagem a cada 10 segundos
+}
+
+function plusSlides(n) {
+    slideIndex += n - 1;
+    showSlides();
+}
