a7b124dd9b284339bd55485c7cdeb2bd62252c56
GuilhermeMth <Guilhermemfranca06@gmail.com>
2025-02-26 22:21:24 -0300

    fix: corrigindo os caminhos dos arquivos em cadastro.php

--- pages/cadastro.php
+++ pages/cadastro.php
@@ -59,8 +59,8 @@ if (isset($_POST['submit'])) {
          <div class="nav">
            <img class="mqa" src="../imgs/MQA_white.svg" alt="">
            <div class="nav-links">
-           <a class="nav-link" href="../html/login.php">Login</a>
-           <a class="nav-link" href="../html/cadastro.php">Sign-up</a>
+           <a class="nav-link" href="../pages/login.php">Login</a>
+           <a class="nav-link" href="../pages/cadastro.php">Sign-up</a>
            </div>
          </div>
       </div>
