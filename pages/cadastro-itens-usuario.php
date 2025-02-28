1c960432678c5fa7dec1268a2533375134f092eb
GuilhermeMth <Guilhermemfranca06@gmail.com>
2025-02-27 21:06:44 -0300

    feat: card para escolher tipo de doação

---
+++ pages/cadastro-itens-usuario.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/itensUsuario.css">
+    <link rel="shortcut icon" href="../imgs/MQA_blue.svg" type="image/x-icon">
+    <title>Login</title>
+</head>
+<body>
+   <div class="app"> <!-- classe app é a div pai de toda pagina -->
+      <div class="header">
+         <div class="nav">
+           <img class="mqa" src="../imgs/MQA_whitewithtext.svg" alt="">
+           <div class="nav-links"> <!-- links do header -->
+           </div>
+         </div>
+      </div>
+      <div class="ong-profile">
+        <div class="ong-profile-space">
+            <div class="ong-profile-img">
+                <img class="ong-profile-img-logo" src="../imgs/avatar.png" alt="">
+            </div>
+            <div class="ong-profile-info">
+                <h1 class="ong-profile-title">Salva Animal</h1>
+            </div>
+        </div>
+      </div>
+      <div class="main"> <!-- parte principal da pagina -->
+        <div class="donatetype-card">
+            <div class="dnttp-title">
+               <h1 class="dnttp-card-title">O que você deseja doar para: ONG Salva Animal</h1>
+            </div>
+            <div class="dnttp-buttons">
+                <button class="dnttp-money">Dinheiro</button>
+                <button class="dnttp-itens">Itens</button>
+            </div>
+        </div>
+   </div>
+</body>
+</html>
