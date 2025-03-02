114b0dfcdced5f210138a140f217ed6068bddc5f
rxl@discente.ifpe.edu.br <rxl@discente.ifpe.edu.br>
2025-03-02 18:45:52 -0300

    Página do usuário logado atualizada com CSS

--- pages/user_logado.php
+++ pages/user_logado.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User_logado</title>
-    <link rel="stylesheet" href="user_logado.css">
+    <link rel="stylesheet" href="../css/user_logado.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Krona+One&family=Lalezar&display=swap" rel="stylesheet">
