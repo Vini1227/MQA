005891a677e80d228103078a54e3f4c06c19fed1
rxl@discente.ifpe.edu.br <rxl@discente.ifpe.edu.br>
2025-03-02 18:49:51 -0300

    Página de conhecimento

---
+++ pages/pag_conhecimento.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pag_conhecimento</title>
+    <link rel="stylesheet" href="../css/pag_conhecimento.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Krona+One&family=Lalezar&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
+  
+</head>
+<body>
+
+    <header>
+        <img src="./img/logov2.png" alt="">
+
+        <div class="link">
+            <a href="#" id="b3">Login</a>
+            <a href="#" id="b4">Cadastre-se</a>
+        </div>
+    </header>
+    
+    <div class="content">
+        <h1>Quem é você ?</h1>
+
+        <div class="conhec">
+
+            <button class="doador">
+                <img src="../imgs/doador.png" alt=""> 
+                <p>Doador</p>
+            </button>
+
+            <button class="inst">
+                <img src="../imgs/ONG.png" alt="">
+                <p>ONG</p>
+            </button>
+        </div>
+           
+
+    </div>
+</body>
+</html>
