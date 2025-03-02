d7985dba44c05ca7760cedd4119159d2967a5416
rxl@discente.ifpe.edu.br <rxl@discente.ifpe.edu.br>
2025-03-02 18:36:01 -0300

    Página de usuário logado

---
+++ pages/user_logado.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User_logado</title>
+    <link rel="stylesheet" href="user_logado.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Krona+One&family=Lalezar&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+
+<body>
+    <header>
+        <img class="v3" src="./img/logov3.png" alt="">
+
+        <div class="link">
+            <a href="#" id="b3">PauloRC0</a>
+            <a href="#" id="b4">Sair</a>
+        </div>
+            
+    </header>
+
+    <div class="content-pesq">
+        <input type="search" class="pesq" placeholder="Procure por uma ONG">
+        <button class="icone">
+            <i class="fas fa-search"></i>
+        </button>
+    </div>
+
+    <div class="opcoes_por_cor">
+
+        <button class="cor" id="01">
+            <img src="./img/cor01.png" alt="">
+        </button>
+
+        <button class="cor" id="02">
+            <img src="./img/cor01.png" alt="">
+        </button>
+
+        <button class="cor" id="03">
+            <img src="./img/cor01.png" alt="">
+        </button>
+
+        <button class="cor" id="04">
+            <img src="./img/cor01.png" alt="">
+        </button>
+
+        <button class="cor" id="05">
+            <img src="./img/cor01.png" alt="">
+        </button>
+
+        <button class="cor" id="06">
+            <img src="./img/cor01.png" alt="">
+        </button>
+
+        <button class="cor" id="07">
+            <img src="./img/cor01.png" alt="">
+        </button>
+
+        <button class="cor" id="08">
+            <img src="./img/cor01.png" alt="">
+        </button>
+
+        <button class="cor" id="09">
+            <img src="./img/cor01.png" alt="">
+        </button>
+
+        <button class="cor" id="10">
+            <img src="./img/cor01.png" alt="">
+        </button>
+
+    </div>
+
+    <div class="title">
+        <h1>Descubra ONGS</h1>
+    </div>
+
+    <div class="content-ongs">
+
+        <div class="card_ong">
+            <img src="./img/coelho1.png" alt="">
+            <button class="botao-ong">Salva Animal</button>
+        </div>
+
+        <div class="card_ong">
+            <img src="./img/cachorro.png" alt="">
+            <button class="botao-ong">Canil Igarassu</button>
+        </div>
+
+        <div class="card_ong">
+            <img src="./img/bromelia.png" alt="">
+            <button class="botao-ong">Refúgio das Bromelias</button>
+        </div>
+    </div>
+
+</body>
+</html>
