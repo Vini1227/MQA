f9c7dbe7d4e15e3ec2fb5920d870d8653ee6a3be
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-03-12 17:24:36 -0300

    feat: limite de ong e deixando aleatorio

--- pages/cards.php
+++ pages/cards.php
@@ -1,18 +1,15 @@
 <?php
 require_once('config.php');
 
-$ongs = [''];
-
-if (!isset($_SESSION['usuario'])) {
-    header('Location: ./login.php');
-    exit();
-}
-
-$usuario = $_SESSION['usuario'];
-
 // Consulta apenas o banner e o nome das ONGs
 $sql = "SELECT nome, banner FROM ongs";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $ongs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Embaralha as ONGs
+shuffle($ongs);
+
+// Pega no máximo 5 ONGs
+$ongs = array_slice($ongs, 0, 5);
 ?>
